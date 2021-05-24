cc96fc08e9ce18efd4ecb66cdc42678af0594993
James Wallis <j@wallis.dev>
2021-05-24 17:15:33 +0100

    Add key to navigation
    Signed-off-by: James Wallis <j@wallis.dev>

--- components/Navigation.js
+++ components/Navigation.js
@@ -17,7 +17,7 @@ const Navigation = () => {
         <nav className="w-screen flex justify-center align-center border-b py-2">
             {
                 links.map(({ name, path, active }) => (
-                    <Link href={path}>
+                    <Link href={path} key={path}>
                         <a className={`py-0 mb-1 md:mb-3 md:mt-4 mt-1 mx-4 ${active ? 'border-b-2 border-button-primary-bg' : ''}`}>
                             {name}
                         </a>
